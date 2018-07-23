1a5821e5b46f536b16bdc48a9d874226537d7ccc
higumachan <harekumo792154@gmail.com>
2018-07-24 00:16:18 +0900

    function division

--- main.go
+++ main.go
@@ -12,10 +12,19 @@ import (
 )
 
 
-func main() {
-	var name = os.Args[1]
-	var args = os.Args[2:]
+func PostSlack(programName string, commandError error)  {
+	api := slack.New(os.Getenv("SLACK_TOKEN"))
+	username := os.Getenv("SLACK_USER_ID")
+	var message string
+	if commandError == nil {
+		message = fmt.Sprintf("<@%s> finished %s", username, programName)
+	} else {
+		message = fmt.Sprintf("<@%s> failed %s %s", username, programName, commandError)
+	}
+	api.PostMessage(os.Getenv("SLACK_CHANNEL_ID"), message, slack.PostMessageParameters{})
+}
 
+func RunCommandAndStreamOutputStdout(name string, args []string) error {
 	cmd := exec.Command(name, args...)
 
 	stdout, err := cmd.StdoutPipe()
@@ -39,15 +48,13 @@ func main() {
 		fmt.Println(string(line))
 	}
 
-	err = cmd.Wait()
+	return cmd.Wait()
+}
 
-	api := slack.New(os.Getenv("SLACK_TOKEN"))
-	username := os.Getenv("SLACK_USER_ID")
-	var message string
-	if err == nil {
-		message = fmt.Sprintf("<@%s> finished %s", username, name)
-	} else {
-		message = fmt.Sprintf("<@%s> failed %s %s", username, name, err)
-	}
-	api.PostMessage(os.Getenv("SLACK_CHANNEL_ID"), message, slack.PostMessageParameters{})
+func main() {
+	var name = os.Args[1]
+	var args = os.Args[2:]
+
+	err := RunCommandAndStreamOutputStdout(name, args)
+	PostSlack(name, err)
 }
