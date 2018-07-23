5ba5e4cf731b78260e1080a28f293d1bb52f5213
higumachan <harekumo792154@gmail.com>
2018-07-23 23:59:46 +0900

    fix bug

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/nlopes/slack"
 	"fmt"
+	"log"
+	"bufio"
+	"io"
 )
 
 
@@ -15,7 +18,29 @@ func main() {
 
 	cmd := exec.Command(name, args...)
 
-	err := cmd.Run()
+	stdout, err := cmd.StdoutPipe()
+	rd := bufio.NewReader(stdout)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer stdout.Close()
+	for {
+		line, _, err := rd.ReadLine()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(line))
+	}
+
+	err = cmd.Wait()
+
 	api := slack.New(os.Getenv("SLACK_TOKEN"))
 	username := os.Getenv("SLACK_USER_ID")
 	var message string
